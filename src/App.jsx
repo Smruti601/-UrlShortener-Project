6c20818ab960f044e8781a01b6c08f5088eaa62a
Smruti Ranjan <smrutisahoo7014@gmail.com>
2025-05-30 23:48:11 +0530

    Landing Page is upadated

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
-
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
        <BrowserRouter>
+       <NavBar />
             <Routes>
               <Route path='/' element={<LandingPage/>} />
               <Route path='/about' element={<AboutPage/>} />
 
             </Routes>
+        <Footer />
        </BrowserRouter>
     </>
   )
